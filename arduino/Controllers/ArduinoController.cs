f540064cbbac1f909e15b3cfaa2924696e6182ee
Ivan Paradžik <ivanparadzik4@gmail.com>
2022-08-29 12:40:00 +0200

    post metoda

--- arduino/Controllers/ArduinoController.cs
+++ arduino/Controllers/ArduinoController.cs
@@ -27,10 +27,10 @@ namespace arduino.Controllers
         [HttpGet]
         [Produces("application/json")]
         [Route("is-enabled")]
-        public async Task<IActionResult> GetIsOn()
+        public async Task<IActionResult> GetIsOnAsync()
         {
             bool isEnabled = false;
-            float temperature=0;
+            float temperature = 0;
             using (NpgsqlConnection con = GetConnection())
             {
                 string sql = "SELECT isenabled,temperature FROM toggleinfo WHERE id = 0";
@@ -53,9 +53,41 @@ namespace arduino.Controllers
             });
         }
 
+        [HttpPost]
+        [Produces("application/json")]
+        [Route("post-info")]
+        public async Task<IActionResult> PostInfoAsync(float temp, float tempSensor, bool ison)
+        {
+            using (NpgsqlConnection con = GetConnection())
+            {
+                var dateCreated = DateTime.Now;
+                string sql = $"INSERT INTO temperaturetime (temp, tempsensor, datecreated, ison) VALUES (:temp, :tempsensor, :datecreated, :ison)";
+                con.Open();
+
+                using (NpgsqlCommand command = new NpgsqlCommand(sql, con))
+                {
+                    try
+                    {
+                        command.Parameters.AddWithValue("temp", temp);
+                        command.Parameters.AddWithValue("tempsensor", tempSensor);
+                        command.Parameters.AddWithValue("datecreated", dateCreated);
+                        command.Parameters.AddWithValue("ison", ison);
+                        await command.ExecuteNonQueryAsync();
+                    }
+                    catch (Exception ex)
+                    {
+                        throw;
+                    }
+                }
+            }
+            return Ok();
+        }
+
         private static NpgsqlConnection GetConnection()
         {
-            return new NpgsqlConnection("Server=tyke.db.elephantsql.com;Database=rrduyjdv;User Id=rrduyjdv;Password=caxhs8GGaKovQ59S5LQFxpPAg1p2Ezwt");
+            var con = new NpgsqlConnection("Server=tyke.db.elephantsql.com;Database=rrduyjdv;User Id=rrduyjdv;Password=caxhs8GGaKovQ59S5LQFxpPAg1p2Ezwt");
+            con.Settings.KeepAlive = 300;
+            return con;
         }
 
         #endregion Methods
